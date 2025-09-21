8baf7db64da84f7b3c4b9b6a3320813b6aa1f1cf
Thouma <richard_allcca_llano@hotmail.com>
2025-09-21 13:14:05 -0500

    feat: add Command component and integrate it into the dashboard layout

---
+++ src/app/dashboard/command/page.tsx
@@ -0,0 +1,97 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+import {
+  Calculator,
+  Calendar,
+  CreditCard,
+  Settings,
+  Smile,
+  User,
+} from "lucide-react";
+
+// import Autoplay from "embla-carousel-autoplay"
+
+export default function Page() {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "j" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 p-8">
+      <h3>Para ver opciones</h3>
+      <p className="text-muted-foreground text-sm" onClick={() => setOpen(true)}>
+        Press{" "}
+        <kbd className="bg-muted text-muted-foreground pointer-events-none inline-flex h-5 items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100 select-none">
+          <span className="text-xs">⌘</span>J
+        </kbd>
+      </p>
+
+      <CommandDialog className="rounded-lg border shadow-md md:min-w-[450px]" open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem onSelect={() => {
+              setOpen(false)
+              alert("Calendar selected")
+            }}>
+              <Calendar />
+              <span>Calendar</span>
+            </CommandItem>
+            <CommandItem onSelect={() => {
+              setOpen(false)
+              alert("Search Emoji selected")
+            }}>
+              <Smile />
+              <span>Search Emoji</span>
+            </CommandItem>
+            <CommandItem disabled>
+              <Calculator />
+              <span>Calculator</span>
+            </CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <CommandItem>
+              <User />
+              <span>Profile</span>
+              <CommandShortcut>⌘P</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <CreditCard />
+              <span>Billing</span>
+              <CommandShortcut>⌘B</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <Settings />
+              <span>Settings</span>
+              <CommandShortcut>⌘S</CommandShortcut>
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+
+    </div>
+  );
+}
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -16,6 +16,7 @@ const links = [
   { name: "card", href: "/dashboard/card" },
   { name: "carousel", href: "/dashboard/carousel" },
   { name: "checkbox", href: "/dashboard/checkbox" },
+  { name: "command", href: "/dashboard/command" },
   { name: "dialog", href: "/dashboard/dialog" },
 ];
 
