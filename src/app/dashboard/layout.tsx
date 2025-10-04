73d7a18c1395a5eba42ae5a68d08424148d53f99
Thouma <richard_allcca_llano@hotmail.com>
2025-10-04 14:27:37 -0500

    feat: add theme toggle functionality; implement ModeToggle and ThemeProvider components

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,6 +2,7 @@
 // https://tailwindcomponents.com/component/tailwind-css-admin-dashboard-layout
 // https://gist.github.com/Klerith/3949f1c8b884d7101e378dfb668f0f3a
 
+import { ModeToggle } from "@/components/modeToggle";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
 import { Toaster as SonnerToaster } from "sonner";
@@ -40,10 +41,13 @@ export default function DashboardLayout({
 }) {
   return (
     <>
-      <nav className="bg-white border-b border-gray-200 fixed z-30 w-full">
+      <nav className="border-gray-200 fixed z-30 w-full bg-background">
         <div className="px-3 py-3 lg:px-5 lg:pl-3">
           <div className="flex items-center justify-between">
+
             <div className="flex items-center justify-start">
+
+              {/* Hamburger button */}
               <button
                 id="toggleSidebarMobile"
                 aria-expanded="true"
@@ -77,6 +81,8 @@ export default function DashboardLayout({
                   ></path>
                 </svg>
               </button>
+
+              {/* Logo */}
               <a
                 href="#"
                 className="text-xl font-bold flex items-center lg:ml-2.5"
@@ -117,8 +123,12 @@ export default function DashboardLayout({
                 </span>
               </a>
             </div>
+
+            {/* Toggle mode */}
+            <ModeToggle />
+
+            {/* User Avatar */}
             <div className="flex items-center">
-              {/* User Avatar */}
               <div className="bg-blue-500 text-white rounded-full w-12 h-12 flex items-center justify-center shadow-md">
                 <Avatar className="w-12 h-12">
                   <AvatarImage
@@ -129,24 +139,26 @@ export default function DashboardLayout({
                 </Avatar>
               </div>
             </div>
+
           </div>
         </div>
       </nav>
-      <div className="flex overflow-hidden bg-white pt-16">
+
+      <div className="flex overflow-hidden pt-16">
         <aside
           id="sidebar"
           className="fixed hidden z-20 h-full top-0 left-0 pt-16 lg:flex flex-shrink-0 flex-col w-64 transition-width duration-75"
           aria-label="Sidebar"
         >
-          <div className="relative flex-1 flex flex-col min-h-0 borderR border-gray-200 bg-white pt-0">
+          <div className="relative flex-1 flex flex-col min-h-0 borderR border-gray-200 pt-0">
             <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
-              <div className="flex-1 px-3 bg-white divide-y space-y-1">
+              <div className="flex-1 px-3 divide-y space-y-1 bg-background text-foreground">
                 <ul className="space-y-2 pb-2">
                   {links.map((link) => (
                     <li key={link.name}>
                       <Link
                         href={link.href}
-                        className="text-base capitalize text-gray-900 font-normal rounded-lg flex items-center p-2 hover:bg-gray-100 group"
+                        className="text-base capitalize font-normal rounded-lg flex items-center p-2 hover:bg-gray-100 dark:hover:bg-gray-500 group"
                       >
                         <span className="ml-3">{link.name}</span>
                       </Link>
@@ -157,18 +169,19 @@ export default function DashboardLayout({
             </div>
           </div>
         </aside>
+
         <div
           className="bg-gray-900 opacity-50 hidden fixed inset-0 z-10"
           id="sidebarBackdrop"
         ></div>
         <div
           id="main-content"
-          className="h-full w-full bg-gray-200 relative overflow-y-auto lg:ml-64"
+          className="h-full w-full bg-gray-200 dark:bg-gray-500 relative overflow-y-auto lg:ml-64"
         >
           <main>
             <div className="pt-6 px-4">
               <div className="w-full min-h-[calc(100vh-230px)]">
-                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
+                <div className="shadow rounded-lg p-4 sm:p-6 xl:p-8 bg-background text-foreground">
                   {children}
                 </div>
               </div>
@@ -177,7 +190,7 @@ export default function DashboardLayout({
 
           <SonnerToaster position="top-center" richColors />
 
-          <footer className="bg-white md:flex md:items-center md:justify-between shadow rounded-lg p-4 md:p-6 xl:p-8 my-6 mx-4">
+          <footer className="md:flex md:items-center md:justify-between shadow rounded-lg p-4 md:p-6 xl:p-8 my-6 mx-4 bg-background text-foreground">
             <ul className="flex items-center flex-wrap mb-6 md:mb-0">
               <li>
                 <a
@@ -289,7 +302,7 @@ export default function DashboardLayout({
               </a>
             </div>
           </footer>
-          <p className="text-center text-sm text-gray-500 my-10">
+          <p className="text-center text-sm text-gray-500 my-10 bg-background text-foreground">
             &copy; 2019-{new Date().getFullYear()}{" "}
             <a href="#" className="hover:underline" target="_blank">
               Themesberg
